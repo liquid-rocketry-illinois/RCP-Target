0904a6eeb9e52415610a4eb8f7c723cf14608c85
Jacob Baumel <jacob.a.baumel@gmail.com>
2025-08-29 18:10:43 -0500

    Added python script to generate version info on platformio install

---
+++ piogenversion.py
@@ -0,0 +1,20 @@
+ver = ""
+with open("VERSION", 'r') as file:
+    ver = file.read()
+
+lines = [
+    "#ifdef __cplusplus\n",
+    "extern \"C\" {\n",
+    "#endif\n",
+    "extern const char* const RCPT_VERSION;\n",
+    "extern const char* const RCPT_VERSION_END;\n",
+    "const char* const RCPT_VERSION = \"" + ver + "\";\n",
+    "const char* const RCPT_VERSION_END = RCPT_VERSION + " + str(len(ver)) + ";\n",
+    "#ifdef __cplusplus\n",
+    "}\n",
+    "#endif\n"
+]
+
+with open("src/VERSION.cpp", 'w') as file:
+    file.writelines(lines)
+
